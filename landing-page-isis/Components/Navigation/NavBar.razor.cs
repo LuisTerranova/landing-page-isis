aa8b75946ccdc10779dd00348c1de723c441751f
LuisTerranova <linharesdev43@gmail.com>
2026-02-02 19:01:06 -0400

    Finished base design

---
+++ landing-page-isis/Components/Navigation/NavBar.razor.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Components;
+
+namespace landing_page_isis.Components.Navigation;
+
+public partial class NavBar : ComponentBase
+{
+}
--- landing-page-isis/Data/AppDbContext.cs
+++ landing-page-isis/Data/AppDbContext.cs
@@ -4,16 +4,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace landing_page_isis.Data;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) 
+public class AppDbContext(DbContextOptions<AppDbContext> options)
     : DbContext(options)
 {
     public DbSet<Pacient> Pacients { get; set; }
     public DbSet<Lead> Leads { get; set; }
     public DbSet<User> Users { get; set; }
-    
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
-};
+}
